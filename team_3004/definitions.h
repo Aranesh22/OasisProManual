d0602fd2a36ec9af4d6922fbdb31caed16f4071a
HumptyDu <shadyashraf2002@gmail.com>
2022-03-26 16:52:27 -0400

    updated gitignore and definitions

--- team_3004/definitions.h
+++ team_3004/definitions.h
@@ -1,7 +1,7 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-enum CEStype {a,b,c};
+enum CEStype {pulse, cycle};
 enum ConnectionState {max, high, medium, adequate, low, critically_low, none};
 
 #endif // DEFINITIONS_H
