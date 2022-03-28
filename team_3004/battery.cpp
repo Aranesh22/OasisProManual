0c84a0d8214bac43d9ee46f4ac5cab4dbc4e51bb
Lindiks <lindsay.dickson@outlook.com>
2022-03-28 19:03:01 -0400

    Update battery.cpp

--- team_3004/battery.cpp
+++ team_3004/battery.cpp
@@ -12,11 +12,30 @@ int Battery::getBatteryPercent() { return batteryPercent; }
 int Battery::getBatteryLevel()   { determineBatteryLevel();
                                    return batteryLevel; }
 
-
+//decremented by 12
 void Battery::determineBatteryLevel() {
+
+// Prob find a cleaner way to code this
     if (batteryPercent>87)
         batteryLevel=8;
-    else
-        batteryLevel=0;
+    else if (batteryPercent>75)
+        batteryLevel=7;
 
+    else if (batteryPercent>63)
+        batteryLevel=6;
+
+    else if (batteryPercent>51)
+        batteryLevel=5;
+
+    else if (batteryPercent>39)
+        batteryLevel=4;
+
+    else if (batteryPercent>27)
+        batteryLevel=3;
+
+    else if (batteryPercent>15)
+        batteryLevel=2;
+
+    else
+        batteryLevel=1;
 }
