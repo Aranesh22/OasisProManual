50ce848a3e60a5ab9753f00eb1c13838238712b5
Rishie <rishie2001@gmail.com>
2022-03-28 19:28:39 -0400

    added functions to source

--- team_3004/session.cpp
+++ team_3004/session.cpp
@@ -2,11 +2,33 @@
 #include "sessionlength.h"
 #include "sessiontype.h"
 
-Session::Session(SessionLength* sl, SessionType* st, int i)
+Session::Session(SessionLength* sl, SessionType* st)
 {
     leftActive = false;
     rightActive = false;
-    intensity = i;
+    intensity = 0;
     length = sl;
     type = st;
+}
+
+void Session::increaseIntensity() {
+    //Max intensity = 8
+    if (intensity < 8) {
+        intensity += 1;
+    }
+}
+
+void Session::decreaseIntensity() {
+    //Lowest intensity = 0
+    if (intensity > 0) {
+        intensity -= 1;
+    }
+}
+
+void Session::changeSessionType(SessionType * newSt) {
+    type = newSt;
+}
+
+void Session::changeSessionLength(SessionLength * newSl) {
+    length = newSl;
 }
