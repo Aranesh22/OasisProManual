61c8f3d39739599846bddb7feffb0a5691aae7f8
Lindiks <lindsay.dickson@outlook.com>
2022-03-28 17:00:17 -0400

    Update battery.h

--- team_3004/battery.h
+++ team_3004/battery.h
@@ -4,10 +4,22 @@
 //hello
 //hi female dog (shady) cxZcxzc
 
+//Assume that there will be a seperate screen that can set the inital battery level when the device turns on
+// For now, whenever the device turns on it will be set to 100% battery
+
+
 class Battery
 {
 public:
     Battery();
+    battery_drain();
+
+
+    getBattery_level();
+
+private:
+    int battery_level;
+
 };
 
 #endif // BATTERY_H
