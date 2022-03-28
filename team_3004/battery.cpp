9abf8c5799e0d6a6e01f68063ae9b8e64735a83f
Lindiks <lindsay.dickson@outlook.com>
2022-03-28 18:52:12 -0400

    Update battery.cpp

--- team_3004/battery.cpp
+++ team_3004/battery.cpp
@@ -1,6 +1,22 @@
+
 #include "battery.h"
 
 Battery::Battery()
 {
-    battery_level = 100;
+    batteryPercent = 100;
+    batteryLevel=0;
+    qInfo("battery init");
+}
+
+int Battery::getBatteryPercent() { return batteryPercent; }
+int Battery::getBatteryLevel()   { determineBatteryLevel();
+                                   return batteryLevel; }
+
+
+void Battery::determineBatteryLevel() {
+    if (batteryPercent>87)
+        batteryLevel=8;
+    else
+        batteryLevel=0;
+
 }
