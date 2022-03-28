117ca643e49dbf1d96b125bf005be1242bcd5544
Rishie <rishie2001@gmail.com>
2022-03-28 18:29:17 -0400

    changed session source

--- team_3004/session.cpp
+++ team_3004/session.cpp
@@ -2,10 +2,11 @@
 #include "sessionlength.h"
 #include "sessiontype.h"
 
-Session::Session(SessionLength* sl, SessionType* st, bool l, bool r)
+Session::Session(SessionLength* sl, SessionType* st, bool l, bool r, int i)
 {
     l = false;
     r = false;
+    intensity = i;
     sl = new SessionLength();
     st = new SessionType();
 }
