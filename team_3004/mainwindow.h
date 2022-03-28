d401a556b845d4c82cf73b911f3d8eebc007c7c7
Lindiks <lindsay.dickson@outlook.com>
2022-03-28 19:54:00 -0400

    Update mainwindow.h

--- team_3004/mainwindow.h
+++ team_3004/mainwindow.h
@@ -20,9 +20,10 @@ public:
     void lit();
     void unLit();
     ~MainWindow();
+   void changePixmap(QString, QLabel*);
 
     void show_battery();
-    void delay();
+    void delayBy(int);
     bool icon_ALL_Lit;
     bool dutyCycle_CESsession_icon_lit;
     bool shortPulse_CESsession_icon_lit;
