af9b7f40c9165e14c2197cfb521192d276c3f168
Lindiks <lindsay.dickson@outlook.com>
2022-03-28 17:51:53 -0400

    Update battery.h

--- team_3004/battery.h
+++ team_3004/battery.h
@@ -2,6 +2,7 @@
 #define BATTERY_H
 
 //this is a push INCLUDING the pro file
+#define MAX_BATTERY 100
 
 //Assume that there will be a seperate screen that can set the inital battery level when the device turns on
 // For now, whenever the device turns on it will be set to 100% battery
