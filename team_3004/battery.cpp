c92d4a2f2f7cb79729e93d3d9760736cb06d3591
Lindiks <lindsay.dickson@outlook.com>
2022-03-29 18:38:18 -0400

    create batteryLevelMap

--- team_3004/battery.cpp
+++ team_3004/battery.cpp
@@ -6,39 +6,31 @@ Battery::Battery()
     batteryPercent = 100;
     batteryLevel=0;
     qInfo("battery init");
+    initMap();
+
 }
 
 int Battery::getBatteryPercent() { return batteryPercent; }
 int Battery::getBatteryLevel()   { determineBatteryLevel();
                                    return batteryLevel; }
+void Battery::determineBatteryLevel() { batteryLevel = batteryLevelMap.lowerBound(batteryPercent).value();}
+
+
+void Battery::initMap() {
+//decrements by 12 - prob a more efficent way to do this
+    batteryLevelMap.insert(100, 8);
+    batteryLevelMap.insert(87, 7);
+    batteryLevelMap.insert(75, 6);
+    batteryLevelMap.insert(63, 5);
+    batteryLevelMap.insert(51, 4);
+    batteryLevelMap.insert(39, 3);
+    batteryLevelMap.insert(27, 2);
+    batteryLevelMap.insert(15, 1);
+}
 
 void Battery::drainBattery(){}
 void Battery::chargeBattery(){}
 
-//decremented by 12
-void Battery::determineBatteryLevel() {
 
-// Prob find a cleaner way to code this
-    if (batteryPercent>87)
-        batteryLevel=8;
-    else if (batteryPercent>75)
-        batteryLevel=7;
 
-    else if (batteryPercent>63)
-        batteryLevel=6;
 
-    else if (batteryPercent>51)
-        batteryLevel=5;
-
-    else if (batteryPercent>39)
-        batteryLevel=4;
-
-    else if (batteryPercent>27)
-        batteryLevel=3;
-
-    else if (batteryPercent>15)
-        batteryLevel=2;
-
-    else
-        batteryLevel=1;
-}
