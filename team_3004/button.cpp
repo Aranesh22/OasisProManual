b7c43beec3a44ca09387e382df5f40958d2ffe34
Lindiks <lindsay.dickson@outlook.com>
2022-03-31 21:35:02 -0400

    Update button.cpp

--- team_3004/button.cpp
+++ team_3004/button.cpp
@@ -1,6 +1,11 @@
 #include "button.h"
+#include <QDebug>
 
-Button::Button()
+Button::Button(QString aPath, QString iPath, QLabel* uiLabel)
 {
-
+//    qInfo() << "creating icon";
+    activePath = aPath;
+    inactivePath = iPath;
+    uiElement = uiLabel;
+    state = false;
 }
