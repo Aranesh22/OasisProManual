ed3c6869d4a36c5012ea86f369c477dcc2287734
Rishie <rishie2001@gmail.com>
2022-04-06 19:17:13 -0400

    added load session and delete session to history manager

--- team_3004/historymanager.cpp
+++ team_3004/historymanager.cpp
@@ -11,3 +11,21 @@ void HistoryManager::SaveSession(Session *saveSess) {
     sessions.push_back(saveSess);
 }
 
+Session* HistoryManager::loadSession(Session * toLoad) {
+    for (Session* s : sessions) {
+        if (toLoad == s) {
+            return s;
+        }
+    }
+    return NULL;
+}
+
+Session* HistoryManager::deleteSession(Session* toDelete) {
+    for (Session* s : sessions) {
+        if (toDelete == s) {
+            sessions.erase(sessions.begin() + i);
+            return s;
+        }
+    }
+    return NULL;
+}
