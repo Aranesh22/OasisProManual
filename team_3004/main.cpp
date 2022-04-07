c6b1d24a24426f3536c54382fddb92ddfa43bff9
Rishie <rishie2001@gmail.com>
2022-04-07 17:51:50 -0400

    tested getSessions function from historymanager in main

--- team_3004/main.cpp
+++ team_3004/main.cpp
@@ -9,6 +9,8 @@
 #include "sessionlength.h"
 #include "sessiontype.h"
 #include "session.h"
+#include "historymanager.h"
+#include <vector>
 #include <iostream>
 #include <string>
 
@@ -18,6 +20,26 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     return a.exec();
+
+
+    /*
+    HistoryManager* history = new HistoryManager();
+    SessionLength* sl = new SessionLength(5,false);
+    SessionType* st = nullptr;
+    Session* testSession1 = new Session(sl,st);
+    Session* testSession2 = new Session(sl,st);
+    Session* testSession3 = new Session(sl,st);
+    history->SaveSession(testSession1);
+    history->SaveSession(testSession2);
+    history->SaveSession(testSession3);
+
+    std::vector<Session*> testVector = history->getSessions();
+    for (Session* s : testVector) {
+        qInfo("%d",s->getCurIntensity());
+    }
+
+    qInfo("Hello faggots");
+    */
 
     return 0;
 }
