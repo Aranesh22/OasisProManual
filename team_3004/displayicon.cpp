14ebabe61febd4d1a389203455fbb143967de9ab
Lindiks <lindsay.dickson@outlook.com>
2022-04-07 21:04:18 -0400

    swapIcon
    disregard the timer object + the flashing

--- team_3004/displayicon.cpp
+++ team_3004/displayicon.cpp
@@ -1,12 +1,26 @@
 #include "displayicon.h"
 
+#include <QDebug>
+
+
 DisplayIcon::DisplayIcon(QString litPath, QString dimPath, QPushButton* ui)
 {
     iconMap[lit] = litPath;
     iconMap[dim] = dimPath;
     uiElement = ui;
     illum = dim;
+    // create a timer
+      timer = new QTimer(this);
+
+      // setup signal and slot
+//      timer->setSingleShot(true);
+      connect(timer, SIGNAL(timeout()),
+            this, SLOT(flashs()));
+    isFlashing = true;
+
 }
+
+
 
 //getters
 QString DisplayIcon::getPath(){return iconMap[illum];}
@@ -15,3 +29,37 @@ QPushButton* DisplayIcon::getUiElement(){return uiElement;}
 void DisplayIcon::setIllumState(IllumState newIllum)    {illum = newIllum;}
 
 void DisplayIcon::toggleIllum(){illum = (illum == lit) ? dim : lit;}
+
+int DisplayIcon::flashFor(int n){
+    qInfo("DisplayIcon::flashFor(%d)", n);
+    timer->start(1000);
+        qInfo() << timer->remainingTime();
+    if(n==0){
+        timer->stop();
+    }
+    return n--;
+
+}
+
+void DisplayIcon::swapIcon(){
+    QIcon newIcon = QIcon(getPath());
+    uiElement->setIcon(newIcon);
+}
+
+void DisplayIcon::stopFlash(){
+    timer->stop();
+}
+
+//
+void DisplayIcon::flashs(){
+    qInfo("flash");
+    toggleIllum();
+
+    qInfo() << timer->remainingTime();
+    return;
+
+//    QTimer *blink = new QTimer(this);
+//    connect(blink, SIGNAL(timeout()), this, SLOT(flashButton()));
+//    blink->start(1000);
+
+}
--- team_3004/displayicon.h
+++ team_3004/displayicon.h
@@ -2,21 +2,36 @@
 #define DISPLAYICON_H
 
 #include "definitions.h"
+#include "ui_mainwindow.h"
 
 #include <QPushButton>
+#include <QTimer>
 
-class DisplayIcon
+
+
+
+
+class DisplayIcon : public QObject
 {
+    Q_OBJECT
 public:
     DisplayIcon(QString, QString, QPushButton*);
 
     //getters
     QString getPath();
     QPushButton* getUiElement();
-
+    QTimer *timer;
     void setIllumState(IllumState);
     void toggleIllum();
+    int flashFor(int);
+    void stopFlash();
 
+    bool isFlashing;
+    void swapIcon();
+//    Ui::MainWindow ui;
+
+public slots:
+    void flashs();
 private:
     IllumState illum;
     QString iconMap[2];
