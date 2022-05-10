0c531827c9e78ec79a6d5e67f808cd5962a6c888
Shady <shadyashraf2002@gmail.com>
2022-05-10 12:55:46 -0400

    cleaned up code

--- team_3004/main.cpp
+++ team_3004/main.cpp
@@ -16,29 +16,9 @@
 
 int main(int argc, char *argv[])
 {
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
-
-/*
-    HistoryManager* history = new HistoryManager();
-    SessionLength* sl = new SessionLength(5,false);
-    SessionType* st = nullptr;
-    SessionType* stOne = new Se
-    Session* testSession1 = new Session(sl,st);
-    Session* testSession2 = new Session(sl,st);
-    Session* testSession3 = new Session(sl,st);
-    history->SaveSession(testSession1);
-    history->SaveSession(testSession2);
-    history->SaveSession(testSession3);
-
-    std::vector<Session*> testVector = history->getSessions();
-    for (Session* s : testVector) {
-        qInfo("%d",s->getCurIntensity());
-    }
-*/
-
     return 0;
 }
